e82adbbfb715dd21b642bbd8b04b7d775f9ba59c
EasonChen <yschen1124@gmail.com>
2022-11-14 23:26:08 +0800

    [eason][9-structure establish] (#10)

--- main.c
+++ main.c
@@ -1,4 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <fileapi.h>
+
+struct link_list_lead{
+    int howMuchNodeInTheList;
+    struct appointment_book *listFirstNode;
+};
+struct appointment_book{
+    struct storage_individual_character *who;
+    struct storage_individual_character *when;
+    struct storage_individual_character *what;
+    struct storage_individual_character *where;
+    struct appointment_book *next;
+};
+
+struct storage_individual_character{
+    struct storage_character *first;
+    struct storage_individual_character *nextIndividualCharacter;
+};
+
+struct storage_character{
+    char word;
+    struct character_node *nextWord;
+};
+
+void AppointmentBook(void);
+
 int main(){
-    printf("hello");
+    AppointmentBook();
+}
+
+void AppointmentBook(void){
+
 }
