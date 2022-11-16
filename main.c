a9f9b342e72d0eb3c58643a75f19d0b6aafd03e0
EasonChen <yschen1124@gmail.com>
2022-11-16 16:37:36 +0800

    [eason][9-structure establish]

--- main.c
+++ main.c
@@ -9,22 +9,21 @@ struct link_list_lead{
     struct appointment_book *listFirstNode;
 };
 struct appointment_book{
-    struct storage_individual_character *who;
-    struct storage_individual_character *when;
-    struct storage_individual_character *what;
-    struct storage_individual_character *where;
+    struct storage_character *who;
+    struct storage_character *when;
+    struct storage_character *what;
+    struct storage_character *where;
     struct appointment_book *next;
-};
-
-struct storage_individual_character{
-    struct storage_character *first;
-    struct storage_individual_character *nextIndividualCharacter;
 };
 
 struct storage_character{
     char word;
-    struct character_node *nextWord;
+    struct storage_character *nextWord;
 };
+
+typedef struct link_list_lead lead;
+typedef struct appointment_book appBook;
+typedef struct storage_character vocab;
 
 void AppointmentBook(void);
 
